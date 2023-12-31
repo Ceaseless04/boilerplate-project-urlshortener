23e175253d7fe9aa5e4c6131dbdb259e0188580f
Ceaseless04 <kristian120304@gmail.com>
2023-12-31 13:14:10 -0500

    finished the url shorterner for the freecodecamp project

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const url_parser = require('url');
+const dns = require('dns');
+
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db('url_shorterner');
+const collection = db.collection('url_post');
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,9 +24,36 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// POST handle request
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  const dnslookup = dns.lookup(url_parser.parse(url).hostname, async function(err, address) {
+    if(err) res.json({ error: 'invalid url' });
+    else {
+      const url_counter = await collection.countDocuments({});
+      const url_document = {
+        url: url,
+        short_url: url_counter
+      }
+
+      const result = await collection.insertOne(url_document);
+      console.log(result);
+      res.json({
+        original_url: url,
+        short_url: url_counter
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:short_url', async function(req, res) {
+  const shortUrl = req.params.short_url;
+  const urlDoc = await collection.findOne({
+    short_url: +shortUrl
+  });
+
+  res.redirect(urlDoc.url);
 });
 
 app.listen(port, function() {
